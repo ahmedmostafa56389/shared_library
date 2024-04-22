c9f4a84a32fa1f9db27853ecbd8f0eb7e1ddeca3
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-04-22 10:12:56 +0200

    Update PushDockerimage.groovy

--- vars/PushDockerimage.groovy
+++ vars/PushDockerimage.groovy
@@ -1,7 +1,7 @@
 #!usr/bin/env groovy
 def call(String Dockerhub , String imageName) {
 
-     withCredentials([usernamePassword(credentialsId: "${Dockerhub}" , usernameVariable: 'USERNAME' , passwordVariable: 'PASWWORD')]){
+     withCredentials([usernamePassword(credentialsId: "${Dockerhub}" , usernameVariable: 'USERNAME' , passwordVariable: 'PASSWORD')]){
           sh ' docker login -u ${USERNAME} -p ${PASSWORD} '
      }
      sh ' docker push ${imageName} '
