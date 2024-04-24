0cecd7a4c3ca328c5a921c1452c8b65a809807c0
ahmed mostafa saad <119954278+ahmedmostafa56389@users.noreply.github.com>
2024-04-24 13:44:54 +0200

    Update EditImageName.groovy

--- vars/EditImageName.groovy
+++ vars/EditImageName.groovy
@@ -1,4 +1,4 @@
 #! usr/bin/env groovy
 def call (String imageName) {
-  sh "sed -i 's|image:.*|image:${imageName}:${BUILD_NUMBER}|g' deploym.yaml"
+  sh "sed -i 's|image:.*|image:ahmedmoo/nti:${BUILD_NUMBER}|g' deploym.yaml"
 }
